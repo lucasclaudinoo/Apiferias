7e15f10841b7cb5cb6f03ba0d5686f772e177509
Lucas Botinelly <lucas.botinelly@grupoicts.com.br>
2022-08-04 09:52:16 -0400

    dalhe claudio

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from imp import reload
 from fastapi import FastAPI
 import uvicorn 
 from server.routes.user import router as UserRouter
@@ -8,5 +7,4 @@ app = FastAPI()
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 
 if __name__ == '__main__':
-    uvicorn.run (app, host='127.0.0.1', port=8005, debug=True)
-    print("running")
+    uvicorn.run(app, host='0.0.0.0', port=8005, debug=True)
--- server/routes/user.py
+++ server/routes/user.py
@@ -4,25 +4,19 @@ from server.database.database import db
 from server.models.user import UserSchema, UserSchemaUpdate
 
 
-
-
 router = APIRouter()
 user_collection = db["user"]
 
- 
-@router.post("/user/create")
-async def create_user(user: UserSchema):
-    user_create = user_collection.find_one({"email": user.email})
-    if user_create is not None:
-        user = user.__dict__
-        return ("O email: ", user["email"],"já existe")
-    else:
-        user = user.__dict__
-        user_collection.insert_one(user)
-        user["_id"] = str(user["_id"])
-        return (user, "Usuario criado com sucesso")
 
-@router.get("/users")
+@router.post("/create")
+async def create_user(user: UserSchema) -> str:
+    if user_collection.find_one({"email": user.email}):
+        return "O email " + user.email + " já existe"
+    return "Usuario criado com sucesso com id: " +\
+        str(user_collection.insert_one(user.__dict__).inserted_id)
+
+
+@router.get("/get_users")
 async def get_users():
     users = user_collection.find()
     users = [user for user in users]
@@ -31,13 +25,14 @@ async def get_users():
     return users
 
 
-@router.get("/user/{user_id}")
+@router.get("/list_user/{user_id}")
 async def get_user(id: str):
     user = user_collection.find_one({"_id": ObjectId(id)})
     user["_id"] = str(user["_id"])
     return user
 
-@router.delete("/user/{user_id}")
+
+@router.delete("/delete_user/{user_id}")
 async def delete_user(id: str):
     user_delete = user_collection.find_one({"_id": ObjectId(id)})
     if user_delete is None:
@@ -46,11 +41,13 @@ async def delete_user(id: str):
         user_collection.delete_one({"_id": ObjectId(id)})
         return "Usuario deletado com sucesso"
 
-@router.put("/user/{user_id}")
+
+@router.put("/update_user/{user_id}")
 async def update_user(user: UserSchemaUpdate):
     user_update = user_collection.find_one({"_id": ObjectId(id)})
     if user_update is None:
         return ("O usuario não existe")
     else:
-        user_collection.update_one({"_id": ObjectId(id)}, {"$set": user.__dict__})
+        user_collection.update_one({"_id": ObjectId(id)}, {
+                                   "$set": user.__dict__})
         return "Usuario atualizado com sucesso"
