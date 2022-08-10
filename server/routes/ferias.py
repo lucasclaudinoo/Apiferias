b814b5cff8f40bd46ed19ace49f36be0a27c3955
lucas <lucasclaudinodev@gmail.com>
2022-08-10 12:58:32 -0400

    :rocket:

--- server/routes/ferias.py
+++ server/routes/ferias.py
@@ -1,45 +1,71 @@
 from fastapi import APIRouter
 from server.database.database import db
-from server.models.ferias_data import DataFerias
+from server.models.ferias import DataFerias
 from bson import ObjectId
 
+
 router = APIRouter()
+ferias_collection = db["ferias"]
+user_collection = db["user"]
 
 
 @router.post("/create")
-async def create_ferias(ferias: DataFerias, user_id: str):
-    user = db["user"].find_one(filter={"_id": user_id})
-    if user:
-        user.user_id = ObjectId(user.user_id)
-        return "Férias criado com sucesso com id: " +\
-            str(db["ferias"].insert_one(ferias.__dict__).inserted_id)
-    return "Usuário não existe"
+async def create_ferias(ferias: DataFerias, email: str):
+    user = user_collection.find_one(filter={'email': email})
+    if not user:
+        return "O usuario não existe"
+    ferias.user_id = user["_id"]
+    ferias_collection.insert_one(ferias.dict())
+    return "Férias criada com sucesso"
 
-@router.get("/get_ferias/{ferias_id}")
-async def get_ferias(email: str):
-    ferias = db["ferias"].find_one({"email": email})
-    if not ferias:
+
+@router.get("/list_all_ferias")
+async def list_all_ferias():
+    total_ferias = ferias_collection.find()
+    total_ferias = [ferias for ferias in total_ferias]
+    for ferias in total_ferias:
+        ferias["_id"] = str(ferias["_id"])
+        ferias["user_id"] = str(ferias["user_id"])
+    return total_ferias
+
+
+@router.get("list_all_ferias_user/{user_id}")
+async def list_all_ferias_user(user_id: str):
+    ferias = ferias_collection.find(filter={'user_id': ObjectId(user_id)})
+    ferias = [ferias for ferias in ferias]
+    for ferias in ferias:
+        ferias["_id"] = str(ferias["_id"])
+        ferias["user_id"] = str(ferias["user_id"])
+    return ferias
+
+@router.put("/approve_ferias/{ferias_id}")
+async def approve_ferias(email: str, id: str):
+    user = user_collection.find_one(filter={"email": email})
+    if not user["admin"]:
+        return ("O usuario não tem permissão para aprovar férias")
+    if not ferias_collection.find_one({"_id": ObjectId(id)}):
         return ("A ferias não existe")
-    if ferias["status"] == "aguardando":
-        return ("A ferias ainda não foi aprovada")
-    if not ferias["status"]:
-        return ("A ferias não foi aprovada")
-    return ("A ferias foi aprovada pode pegar suas malas!!!")
+    ferias_collection.update_one({"_id": ObjectId(id)}, {
+        "$set": {"status": True}})
+    return ("A ferias foi aprovada")
+
+
+@router.put("/reject_ferias/{ferias_id}")
+async def reject_ferias(email: str, id: str):
+    user = user_collection.find_one(filter={"email": email})
+    if not user["admin"]:
+        return ("O usuario não tem permissão para aprovar férias")
+    if not ferias_collection.find_one({"_id": ObjectId(id)}):
+        return ("A ferias não existe")
+    ferias_collection.update_one({"_id": ObjectId(id)}, {
+        "$set": {"status": False}})
+    return ("A ferias foi rejeitada")
 
 
 @router.delete("/delete_ferias/{ferias_id}")
-async def delete_ferias(email: str):
-    if not db["ferias"].find_one({"email": email}):
+async def delete_ferias(id: str):
+    if not ferias_collection.find_one({"_id": ObjectId(id)}):
         return ("A ferias não existe")
-    db["ferias"].delete_one({"email": email})
+    ferias_collection.delete_one({"_id": ObjectId(id)})
     return ("A ferias foi deletada")
 
-@router.put("/aprove_ferias/{ferias_id}")
-async def aprove_ferias(email: str, user_id: str):
-    user = db["user"].find_one(filter={"_id": user_id})
-    if user["admin"]:
-        if not db["ferias"].find_one({"email": email}):
-            return "A ferias não existe"
-        db["ferias"].update_one({"email": email}, {"$set": {"status": True}})
-        return "A ferias foi aprovada"
-    return "Usuário sem permissão"
